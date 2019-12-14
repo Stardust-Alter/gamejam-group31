ba9e3fb833f6fcf7bc4cfef0315fc95763d113a3
Stardust-Alter <798483752@qq.com>
2019-12-14 14:24:26 +0800

    更新代码

---
+++ Assets/Scripts/GameManagement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagement : MonoBehaviour
+{
+    public static GameManagement _Instance;
+
+    private int health = 100;
+
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            if(health<=0)
+            {
+                GameOver();
+            }
+        }
+    }
+
+
+    void Awake()
+    {
+        _Instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GetHurt(int hurt)
+    {
+        health = -hurt;
+    }
+
+    void GameOver()
+    {
+        //TODO       
+    }
+}
