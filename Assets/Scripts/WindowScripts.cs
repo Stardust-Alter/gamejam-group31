51006afc697a9e3c92094f4f51a9afc61fd79039
NineSong-CN <36152213+NineSong-CN@users.noreply.github.com>
2019-12-14 12:37:40 +0800

    Update Scene

---
+++ Assets/Scripts/WindowScripts.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowScripts : MonoBehaviour
+{
+    public float currentAngle = 0f;
+    public float angleRotateMix = 0f;
+    public float angleRotateMax = 90f;
+    public float rotateSpeed = 360f;
+    public GameObject rotateAxie = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAngle = transform.rotation.y;
+        rotateAxie = GameObject.FindGameObjectWithTag("WindowAxis");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentAngle < angleRotateMax)
+            transform.RotateAround(rotateAxie.transform.position, rotateAxie.transform.up, rotateSpeed * Time.deltaTime);
+    }
+}
